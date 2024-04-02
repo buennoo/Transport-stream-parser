c5134e4833e9a4259d73aa7f3934eaf96d9d4855
buennoo <127790828+buennoo@users.noreply.github.com>
2024-04-02 19:13:00 +0200

    initial commit

---
+++ TS_parser.cpp
@@ -0,0 +1,67 @@
+#include "tsCommon.h"
+#include "tsTransportStream.h"
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <iomanip>
+
+//=============================================================================================================================================================================
+
+int main(int argc, char *argv[ ], char *envp[ ])
+{
+  // TODO - open file
+  std::ifstream file;
+  file.open("example_new.ts", std::ios::binary);
+
+  // TODO - check if file if opened
+  if(!file){
+    std::perror("File opening failed");
+    return EXIT_FAILURE;
+  }
+
+  xTS_PacketHeader    TS_PacketHeader;
+  int32_t TS_PacketId = 0;
+
+  // 188 bajtow - tyle ma caly pakiet
+  char *TS_PacketBuffer = new char[188];
+  uint8_t *Header = new uint8_t[4];
+
+  /*
+  na potem
+  przekastyowac pointer typu uint na pointer typu char
+  char *TS_Packet = reinterpret_cast<char*>(TS_PacketBuffer);
+  */
+
+  while(!file.eof() )
+  {
+    // TODO - read from file
+    file.read(TS_PacketBuffer, 188);
+
+    for(int i = 0; i < 4; i++){
+      Header[i] = (uint8_t)TS_PacketBuffer[i];
+    }
+
+    //reset pol naglowka pakietu strumienia transportowego
+    TS_PacketHeader.Reset();
+    TS_PacketHeader.Parse(Header);
+
+    printf("%010d ", TS_PacketId);
+    TS_PacketHeader.Print();
+    printf("\n");
+
+    if(TS_PacketId == 20){
+      break;
+    }
+    //nastepny pakiet (odczyt co 188 bajtow)
+    TS_PacketId++;
+  }
+  
+  // TODO - close file
+  file.close();
+
+  delete [] TS_PacketBuffer;
+
+  return EXIT_SUCCESS;
+}
+
+//=============================================================================================================================================================================
---
+++ tsTransportStream.cpp
@@ -0,0 +1,67 @@
+#include "tsTransportStream.h"
+#include <iostream>
+
+//=============================================================================================================================================================================
+// xTS_PacketHeader
+//=============================================================================================================================================================================
+
+
+/// @brief Reset - reset all TS packet header fields
+void xTS_PacketHeader::Reset()
+{
+  //write me
+  m_SB = 0;
+  m_E = 0;
+  m_S = 0;
+  m_T = 0;
+  m_PID = 0;
+  m_TSC = 0;
+  m_AFC = 0;
+  m_CC = 0;
+}
+
+/**
+  @brief Parse all TS packet header fields
+  @param Input is pointer to buffer containing TS packet
+  @return Number of parsed bytes (4 on success, -1 on failure) 
+ */
+int32_t xTS_PacketHeader::Parse(const uint8_t* Input)
+{
+  //i`m empty
+  if(Input == nullptr){
+    return -1;
+  }
+
+  // polaczenie 4 pierwszych bajtow do zmiennej (32-bit)
+  //uint32_t *HP = (uint32_t*)Input;
+
+  // zamiana kolejnosci bajtow - najbardziej znaczacy bajt pierwszy big endian
+  int32_t Head = xSwapBytes32(*reinterpret_cast<const uint32_t*>(Input));
+
+  m_SB = (Head & 0xff000000) >> 24;
+  m_E = (Head & 0x800000) >> 23;
+  m_S = (Head & 0x400000) >> 22;
+  m_T = (Head & 0x200000) >> 21;
+  m_PID = (Head & 0x1fff00) >> 8;
+  m_TSC = (Head & 0xc0) >> 6;
+  m_AFC = (Head & 0x30) >> 4;
+  m_CC = Head & 0xf;
+
+  return 4; // Number of parsed bytes
+}
+
+/// @brief Print all TS packet header fields
+void xTS_PacketHeader::Print() const
+{
+  //print sth
+  std::cout << "SB=" << (int)m_SB <<
+    " E=" << (int)m_E <<
+    " S=" << (int)m_S << 
+    " m_T=" << (int)m_T << 
+    " PID=" << (int)m_PID << 
+    " TSC=" << (int)m_TSC << 
+    " AFC=" << (int)m_AFC <<
+    " CC=" << (int)m_CC << std::endl;
+}
+
+//=============================================================================================================================================================================
